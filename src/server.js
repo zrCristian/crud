8e50fdf0189883a95581e6d673541c3c265cfc99
German Eichemberger <germaneichemberger@gmail.com>
2023-08-26 09:28:02 -0300

    set up ejs

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const router = require('./routes');
 
 const app = express();
 const PORT = 8080;
 
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
 app.use(morgan('tiny'));
+
+app.use('/', router);
 
 app.listen(PORT, () => {
   console.log(`[server]: running on port: ${PORT}`);
