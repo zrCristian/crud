5933fc5ce731614aba28bf8e6f05a0cd7b8b2067
German Eichemberger <germaneichemberger@gmail.com>
2023-08-26 09:17:58 -0300

    configures morgan

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 const PORT = 8080;
+
+app.use(morgan('tiny'));
 
 app.listen(PORT, () => {
   console.log(`[server]: running on port: ${PORT}`);
