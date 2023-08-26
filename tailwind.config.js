cea96bb10cec04f950d984010ba76070dc49bd7e
German Eichemberger <germaneichemberger@gmail.com>
2023-08-26 09:55:36 -0300

    configures tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',
+    './src/views/*.ejs',
+    './src/views/**/*.ejs',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
