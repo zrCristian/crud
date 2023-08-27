df690daa8bdda29549b4416f741a6d73422bf103
German Eichemberger <germaneichemberger@gmail.com>
2023-08-27 10:15:59 -0300

    removes morgan

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const morgan = require('morgan');
 const router = require('./routes');
 const coursesRouter = require('./routes/courses');
 
@@ -9,12 +8,11 @@ const PORT = 8080;
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
 
-app.use(morgan('tiny'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', router);
 app.use('/cursos', coursesRouter);
+app.use('/', router);
 
 app.get('*', (req, res) => {
   res.status(404).render('404');
