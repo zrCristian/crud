ff1e8397e35d38d7a79726f9fe912dd7c856b5a2
German Eichemberger <germaneichemberger@gmail.com>
2023-08-27 10:30:00 -0300

    setup class 28

--- src/controllers/courses.js
+++ src/controllers/courses.js
@@ -1,54 +1,15 @@
-const { getCourseById, getPaginatedCourses, getAllCourses } = require('../data/courses');
-const { defaultValues } = require('../utils/constants');
-const { writeJsonWithNewData } = require('../utils/json');
-
 function listAll(req, res) {
-  const page = req.query.page ? (+(req.query.page) - 1) : 0;
-  const courses = getPaginatedCourses(page);
-  const totalNumberOfCourses = getAllCourses().length;
-  const numberOfPages = Math.ceil(totalNumberOfCourses / defaultValues.coursesPerPage);
 
-  res.render('courses/courses', {
-    courses,
-    metadata: {
-      numberOfPages,
-      page: page + 1,
-    },
-  });
 }
 
 function getById(req, res) {
-  const { id } = req.params;
-
-  const course = getCourseById(+id);
-  res.render('courses/course', { course });
 }
 
 function createView(req, res) {
-  res.render('courses/create');
 }
 
 function create(req, res) {
-  const {
-    name, description, price, duration,
-  } = req.body;
 
-  const newCourse = {
-    id: new Date().getTime(),
-    name,
-    description,
-    price: +price,
-    duration: +duration,
-    image: req.file.filename,
-    stars: 0,
-  };
-
-  const courses = getAllCourses();
-  courses.push(newCourse);
-
-  writeJsonWithNewData('courses.json', JSON.stringify(courses));
-
-  res.redirect('/cursos');
 }
 
 module.exports = {
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -1,5 +1,4 @@
 const { Router } = require('express');
-const multer = require('multer');
 const {
   listAll,
   createView,
@@ -9,22 +8,6 @@ const {
 
 const router = Router();
 
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, './public/images/courses');
-  },
-  filename(req, file, cb) {
-    const extension = file.originalname.split('.')[1];
-    cb(null, `${file.fieldname}-${new Date().getTime()}.${extension}`);
-  },
-});
-
-const upload = multer({ storage });
-
 router.get('/', listAll);
-router.get('/crear', createView);
-router.get('/:id', getById);
-
-router.post('/crear', upload.single('image'), create);
 
 module.exports = router;
