bf36d07e15aeac7c7f2515326b550bef8272c952
German Eichemberger <germaneichemberger@gmail.com>
2023-08-29 06:27:12 -0300

    uses env for port

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const usersRouter = require('./routes/users');
 const { styles } = require('./views/css/constants');
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.locals = { styles };
 
