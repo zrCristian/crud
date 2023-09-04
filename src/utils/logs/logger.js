c087a98ce7716730bd10bf8a5233f9f2fbe549bd
German Eichemberger <germaneichemberger@gmail.com>
2023-09-04 08:24:07 -0300

    configures log levels

--- src/utils/logs/logger.js
+++ src/utils/logs/logger.js
@@ -1,6 +1,20 @@
 const { createLogger, transports, format } = require('winston');
+require('dotenv').config();
+
+const logLevel = process.env.LOG_LEVEL || 2;
+
+const LOGS_LEVEL = {
+  0: 'error',
+  1: 'warn',
+  2: 'info',
+  3: 'http',
+  4: 'verbose',
+  5: 'debug',
+  6: 'silly',
+};
 
 const logger = createLogger({
+  level: LOGS_LEVEL[logLevel],
   format: format.combine(
     format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
     format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`),
