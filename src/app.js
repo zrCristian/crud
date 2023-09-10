60ec2b99b4467d923526ad1b899ce099e3dc311d
German Eichemberger <germaneichemberger@gmail.com>
2023-09-10 12:55:44 -0300

    updates import

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 const initDB = require('./config/db');
-const { appConfig } = require('./config/env');
+const { appConfig: { PORT } } = require('./config/env');
 const app = require('./server');
 const logger = require('./utils/logs/logger');
-
-const { PORT } = appConfig;
 
 async function startServer() {
   await initDB();
