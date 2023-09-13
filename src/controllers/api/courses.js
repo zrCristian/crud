c162bf481663f22eac1e5ba2013e30a206b45fb3
German Eichemberger <germaneichemberger@gmail.com>
2023-09-13 11:25:00 -0300

    updates variable name

--- src/controllers/api/courses.js
+++ src/controllers/api/courses.js
@@ -1,11 +1,11 @@
-const courseCategory = require('../../services/CourseService');
+const courseService = require('../../services/CourseService');
 const buildPage = require('../../utils/buildPage');
 const getQueryFromRequest = require('../../utils/getQueryFromReq');
 
 async function getPaginated(req, res) {
   const query = getQueryFromRequest(req);
 
-  const { data, count } = await courseCategory.getPaginated(query);
+  const { data, count } = await courseService.getPaginated(query);
   const response = buildPage(data, count, query.page, query.order);
 
   res.json(response);
