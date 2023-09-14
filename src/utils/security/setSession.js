e9ce458f116560dd4db390835e0f17028fa695f8
German Eichemberger <germaneichemberger@gmail.com>
2023-09-14 08:52:43 -0300

    updates session data

--- src/utils/security/setSession.js
+++ src/utils/security/setSession.js
@@ -1,6 +1,6 @@
 function setSessionWithUserData(req, user) {
   req.session.userId = user.id;
-  req.session.isAdmin = user.isAdmin;
+  req.session.isAdmin = user.role.name === 'Admin';
 }
 
 module.exports = setSessionWithUserData;
